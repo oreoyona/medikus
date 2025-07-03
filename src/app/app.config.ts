d0721de59f2dee2c71b98b8f095e68ebec0d8aa1
oreoyona <81833023+oreoyona@users.noreply.github.com>
2025-07-03 10:53:43 +0200

    Updated to medikus 1.0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth/auth.interceptor';
 import { NgxIndexedDBModule, DBConfig } from 'ngx-indexed-db';
 
@@ -30,7 +30,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled',anchorScrolling: 'enabled',})), 
     provideAnimationsAsync(), 
-    provideHttpClient( withInterceptors([authInterceptor])),
+    provideHttpClient( withInterceptors([authInterceptor]), withFetch()),
     ...(NgxIndexedDBModule.forRoot(dbConfig).providers || []) // Vérification et valeur par défaut
 
     
